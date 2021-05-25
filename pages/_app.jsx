f6147e1964b8cdd1e6cd30aab0cc93dd2dda2a12
Michael Gwynne <votemike@users.noreply.github.com>
2021-05-25 13:53:21 +0100

    Adding ESLint and Prettier (#1)

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
