7415503ef1f02246e2177c80131cc5aebafd575b
Michael Gwynne <michael@votemike.co.uk>
2021-05-26 16:59:20 +0100

    More

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     webpack5: true,
   },
   images: {
-    domains: ["deelay.me"],
+    domains: ["deelay.me"], // Can be deleted after initial project set up. Only used to display slow network requests
   },
 };
