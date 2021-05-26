44d1d6c436095eb46d4e7c461603642712257610
Michael Gwynne <michael@votemike.co.uk>
2021-05-26 09:23:10 +0100

    Adding accessibility check

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,10 @@ module.exports = {
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
+    "@storybook/addon-links",
+  ],
   presets: [path.resolve(__dirname, "./next-preset.js")],
 };
