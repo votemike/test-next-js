c34ccdf88b82dba6b76d1bb023d7dda36cf0744f
Michael Gwynne <michael@votemike.co.uk>
2021-05-26 10:27:57 +0100

    Adding Header and Footer

---
+++ pages/help.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+function Help() {
+  return (
+    <>
+      <Head>
+        <title>Help</title>
+        <meta property="og:title" content="Help" key="title" />
+      </Head>
+      <div className="content">
+        <h1>Help</h1>
+      </div>
+    </>
+  );
+}
+
+export default Help;
