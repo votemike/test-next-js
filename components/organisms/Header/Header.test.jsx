48af3e697f64f27f5c58b7a97cb201157b2c3c96
Michael Gwynne <michael@votemike.co.uk>
2021-06-25 21:10:34 +0100

    Adding some tests

--- components/organisms/Header/Header.test.jsx
+++ components/organisms/Header/Header.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import Header from "./Header";
 
@@ -6,6 +6,25 @@ describe("Header", () => {
   test("renders Header component", () => {
     render(<Header />);
 
-    expect(screen.getByRole("link").text).toBe("Company Name");
+    const links = screen.getAllByRole("link");
+    expect(links[1].text).toBe("Home");
+    expect(links[2].text).toBe("About");
+    expect(links[3].text).toBe("Help");
+    expect(links[4].text).toBe("Legal");
+  });
+
+  test("clicking the menu opens the menu and click again closes it", async () => {
+    render(<Header />);
+    const initialOpenButton = screen.getByLabelText("Open main menu");
+    fireEvent.click(initialOpenButton);
+    const closeButton = await screen.getByLabelText("Close main menu");
+    const afterFirstClickOpenButton = screen.queryByLabelText("Open main menu");
+    expect(closeButton).toBeTruthy();
+    expect(afterFirstClickOpenButton).toBeFalsy();
+    fireEvent.click(closeButton);
+    const finalOpenButton = await screen.getByLabelText("Open main menu");
+    const finalCloseButton = screen.queryByLabelText("Close main menu");
+    expect(finalOpenButton).toBeTruthy();
+    expect(finalCloseButton).toBeFalsy();
   });
 });
